1f6aee276ce0c552803bc8f021bbc24bad2309cb
cymarcaida <cymon.marcaida12@gmail.com>
2024-07-21 09:37:07 +0800

    feat: modified app and main files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ cache: true }), ProductModule],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
