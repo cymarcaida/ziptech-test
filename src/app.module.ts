29679f69a481e8919e2fbfffaa656fb9ff2d2dd1
cymarcaida <cymon.marcaida12@gmail.com>
2024-07-21 13:48:32 +0800

    feat: added of new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
+import { AccountModule } from './account/account.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ cache: true }), ProductModule],
+  imports: [ConfigModule.forRoot({ cache: true }), AuthModule, AccountModule, ProductModule],
   controllers: [],
   providers: [],
 })
