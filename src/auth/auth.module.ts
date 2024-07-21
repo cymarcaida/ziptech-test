2a0c71672bf842b215ba690d8421353c2cc7764c
cymarcaida <cymon.marcaida12@gmail.com>
2024-07-21 13:54:16 +0800

    chore: removed private keys

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { AccountModule } from 'src/account/account.module';
     AccountModule,
     PassportModule,
     JwtModule.register({
-      secret: "1df7ca444b37b2cf9d286e5ffc1e5c81ab448cc7a8ef0e2651e3ca8b27cc5ee609be363cb9588cbd7bf7887e41e8890414a85cbda732db961b0950fd85ed0f4a",
+      secret: process.env.JWT_SECRET_KEY,
       signOptions: { expiresIn: '60m' },
     }),
   ],
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 			ignoreExpiration: false,
-			secretOrKey: "1df7ca444b37b2cf9d286e5ffc1e5c81ab448cc7a8ef0e2651e3ca8b27cc5ee609be363cb9588cbd7bf7887e41e8890414a85cbda732db961b0950fd85ed0f4a",
+			secretOrKey: process.env.JWT_SECRET_KEY,
 		});
 	}
 
